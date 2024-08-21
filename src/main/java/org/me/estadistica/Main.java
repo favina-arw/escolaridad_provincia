f709f78c6e71a337a2a8149aeed9158312a31f09
Federico <favina.arw@gmail.com>
2024-08-21 12:50:47 -0300

    Se hizo mucho

--- src/main/java/org/me/estadistica/Main.java
+++ src/main/java/org/me/estadistica/Main.java
@@ -1,7 +1,97 @@
 package org.me.estadistica;
 
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.me.estadistica.entity.Escolaridad;
+import org.me.estadistica.util.FileGrabber;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            FileGrabber fg = new FileGrabber();
+            String rutaDestino = "";
+
+            if(args.length == 0){
+                Scanner input = new Scanner(System.in);
+                System.out.println("Ingrese la ruta absoluta donde se encuentran los archivos a leer");
+                fg.setPathToFiles(input.nextLine());
+                System.out.println("Ingrese la ruta absoluta donde se deba depositar el archivo resulante");
+                rutaDestino = input.nextLine();
+                System.out.println(rutaDestino);
+                if (rutaDestino.charAt(rutaDestino.length()-1) != '/'){
+                    rutaDestino.concat("/");
+                }
+                System.out.println(rutaDestino);
+            }else{
+                fg.setPathToFiles(args[0]);
+                rutaDestino = args[1];
+            }
+            List<String> archivos = fg.seleccionarArchivos();
+
+            LocalDate fechaActual = LocalDate.now();
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyMMdd");
+            String hoy = fechaActual.format(formato);
+
+            //rutaDestino+"/APRO.CE.XX."+hoy+".txt"
+
+            BufferedWriter escritor = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(rutaDestino+"/CERTESC_90022804_"+hoy+".txt"),"Windows-1252"));
+
+            /*PARA PENSAR*/
+            /*¿CÓMO VAMOS A ENCONTRAR A LOS ALUMNOS?*/
+
+            for (String archivo : archivos){
+                POIFSFileSystem fs = new POIFSFileSystem(new File(archivo));
+                HSSFWorkbook libro = new HSSFWorkbook(fs.getRoot(),true);
+                Sheet hoja = libro.getSheetAt(0);
+
+                Escolaridad escolaridad = new Escolaridad();
+
+                //Cambiar el 0 por la fila en la que comienzan los datos
+                for (int i = 0; i <= hoja.getLastRowNum() ; i++){
+                    Row fila = hoja.getRow(i);
+                    /* INICIO LOGICA DE ARMADO DEL OBJETO ESCOLARIDAD */
+
+
+
+                    /* FIN LOGICA DE ARMADO DEL OBJETO ESCOLARIDAD */
+                    escritor.write(escolaridad.toString());
+                }
+                escritor.write(escolaridad.toString());
+                /*---- CERRDADO DE ARCHIVO DE LECTURA----*/
+                fs.close();
+            }
+            /*---- CERRDADO DE ARCHIVO DE ESCRITURA----*/
+            escritor.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        /*Pruebas de print*/
+
+        /*Escolaridad escolaridadUno = new Escolaridad("260065000","","20380809290",
+                "2024","2","5","","17/06/2024",
+                "",'S','S');
+
+        Escolaridad escolaridadDos = new Escolaridad("260065000","","20380809290",
+                "2024","6","","Tecnicatura Universitaria en Desarrollo de Software","17/06/2024",
+                "03/04/2024","Si","Si");
+
+        System.out.println(escolaridadUno);
+        System.out.println(escolaridadDos);*/
     }
 }
--- src/main/java/org/me/estadistica/entity/Escolaridad.java
+++ src/main/java/org/me/estadistica/entity/Escolaridad.java
@@ -9,13 +9,48 @@ import java.time.format.DateTimeFormatter;
 @Getter(AccessLevel.PUBLIC)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor
-@AllArgsConstructor
 public class Escolaridad {
 
     private static final String FILLER_ZERO = "0";
     private static final String FILLER_SPACE = " ";
     private static final String CODIGO_DEPENDENCIA = "90022804";
     private static final String FIN_TUPLA = "\r\n";
+
+    public Escolaridad(@NonNull String cueAnexo, @NonNull String regice, @NonNull String cuilEstudiante, @NonNull String cicloLectivo, @NonNull String nivel, String gradoAnio, String nombreCursoCarrera, String fechaCertificacion, String fechaInicioCicloLectivo, @NonNull char esEducacionOficial, @NonNull char esAlumnoRegular) {
+        this.cueAnexo = agregarCerosAdelante(cueAnexo, 9);
+        this.regice = agregarCerosAdelante(regice, 10);
+        if(cuilEstudiante.length() > 11){
+            cuilEstudiante.replace("-","").replace(".","");
+        }
+        this.cuilEstudiante = agregarCerosAdelante(cuilEstudiante, 11);
+        this.cicloLectivo = agregarCerosAdelante(cicloLectivo, 4);
+        this.nivel = agregarCerosAdelante(nivel,2);
+        this.gradoAnio = agregarCerosAdelante(gradoAnio,1);
+        this.nombreCursoCarrera = agregarEspaciosAlFinal(nombreCursoCarrera,80);
+        this.fechaCertificacion = agregarCerosAdelante(formatearFechaArgentina(fechaCertificacion),8);
+        this.fechaInicioCicloLectivo = agregarCerosAdelante(formatearFechaArgentina(fechaInicioCicloLectivo),8);
+        this.esEducacionOficial = esEducacionOficial;
+        this.esAlumnoRegular = esAlumnoRegular;
+    }
+
+    public Escolaridad(@NonNull String cueAnexo, @NonNull String regice, @NonNull String cuilEstudiante, @NonNull String cicloLectivo, @NonNull String nivel, String gradoAnio, String nombreCursoCarrera, String fechaCertificacion, String fechaInicioCicloLectivo, @NonNull String esEducacionOficial, @NonNull String esAlumnoRegular) {
+        this.cueAnexo = agregarCerosAdelante(cueAnexo, 9);
+        this.regice = agregarCerosAdelante(regice, 10);
+        if(cuilEstudiante.length() > 11){
+            cuilEstudiante.replace("-","").replace(".","");
+        }
+        this.cuilEstudiante = agregarCerosAdelante(cuilEstudiante, 11);
+        this.cicloLectivo = agregarCerosAdelante(cicloLectivo, 4);
+        this.nivel = agregarCerosAdelante(nivel,2);
+        if (gradoAnio.length() == 0 || gradoAnio.equals(""))
+            this.gradoAnio = "0";
+        else this.gradoAnio = gradoAnio;
+        this.nombreCursoCarrera = agregarEspaciosAlFinal(nombreCursoCarrera,80);
+        this.fechaCertificacion = agregarCerosAdelante(formatearFechaArgentina(fechaCertificacion),8);
+        this.fechaInicioCicloLectivo = agregarCerosAdelante(formatearFechaArgentina(fechaInicioCicloLectivo),8);
+        this.esEducacionOficial = esEducacionOficial.toUpperCase().charAt(0);
+        this.esAlumnoRegular = esAlumnoRegular.toUpperCase().charAt(0);
+    }
 
     @NonNull
     String cueAnexo, regice, cuilEstudiante, cicloLectivo, nivel;
@@ -52,14 +87,14 @@ public class Escolaridad {
     }
 
     public void setGradoAnio(String gradoAnio) {
-        if(this.nivel == "1" || this.nivel == "01" || this.nivel == "2" || this.nivel == "02"){
-            this.gradoAnio = gradoAnio;
+        if(Integer.parseInt(gradoAnio) == 1 || Integer.parseInt(gradoAnio) == 2){
+            this.gradoAnio = agregarCerosAdelante(gradoAnio,2);
         }
     }
 
     public void setFechaInicioCicloLectivo(String fechaInicioCicloLectivo) {
         if(Integer.parseInt(this.nivel) > 5)
-            this.fechaInicioCicloLectivo = fechaInicioCicloLectivo;;
+            this.fechaInicioCicloLectivo = formatearFechaExtrangera(fechaInicioCicloLectivo);;
     }
 
     public void setNombreCursoCarrera(String nombreCursoCarrera) {
@@ -71,7 +106,7 @@ public class Escolaridad {
         //La fecha de certificación, se refiere a la fecha en la que el estableimiento educativo
         //acredita la escolaridad del menor.
         //Debe ser mayor o igual a la fecha de inicio de ciclo lectivo.
-        this.fechaCertificacion = formatearFechaExtrangera(fechaCertificacion);
+        this.fechaCertificacion = this.formatearFechaArgentina(fechaCertificacion);
     }
 
     public void setEsEducacionOficial(@NonNull String esEducacionOficial) {
@@ -97,6 +132,9 @@ public class Escolaridad {
     }
 
     public String formatearFechaArgentina(String fechaInput){
+        if(fechaInput.length() == 0){
+            return "";
+        }
         String fecha = fechaInput.replace("/", "-");
         String pattern = "dd-MM-yyyy";
 
